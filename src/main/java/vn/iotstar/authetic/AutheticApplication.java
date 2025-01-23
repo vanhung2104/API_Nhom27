6061ed06c0dcd626e34761965781c1756edd041a
DangKhoi <pdkhoi2004@gmail.com>
2025-01-23 16:13:52 +0700

    first commit

---
+++ src/main/java/vn/iotstar/authetic/AutheticApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar.authetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutheticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutheticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/iotstar/authetic/AutheticApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.authetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutheticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
